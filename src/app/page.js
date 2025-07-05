cdb166f187aca44fb527dce8f8b4c3dd87a08060
Rimsha Arshad <rimshaarshad374@gmail.com>
2025-07-05 14:27:01 +0500

    hello

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,60 @@
 import Image from "next/image";
+import strawberry from "../../public/image/strawberry.png";
+import chocolate from "../../public/image/chocolate.png";
+import vanilla from "../../public/image/vanilla.png";
+import blue_berry from "../../public/image/blue_berry.png";
+import ice_cream from "../../public/image/main.png";
+import Navbar from "@/compnents/navbar";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="min-h-screen bg-gradient-to-br from-white to-[#c1a089] overflow-x-hidden">
+      <Navbar />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <section className="flex flex-wrap md:flex-nowrap w-full">
+        <div className="w-full md:w-1/2 flex flex-col justify-between">
+          <div className="mb-[0.1rem] px-3 md:px-0 md:pl-[10rem] pt-2 md:pt-5">
+            <h5 className="font-bold text-[0.6rem] md:text-[1rem] text-[#b78664] hover:text-[#9f7354] cursor-pointer pb-2">WELCOME TO</h5>
+            <h2 className="font_style font-bold text-[2.2rem] md:text-[3rem] text-[#8B4513] mb-8 md:mb-10 cursor-pointer tracking-wider">Ice Cream Shop</h2>
+            <p className="text-[#91572d] text-[0.8rem] md:text-lg">Enjoy our rich, creamy ice creams in flavors like chocolate, vanilla, strawberry, and more. Made fresh daily, each scoop is a sweet treat to brighten your day.</p>
+            <div className="mt-3 md:mt-6 flex gap-4">
+              <Link href={"/cart"}><button className="p-[0.5rem] md:p-[0.7rem] bg-[#8B4513] text-[0.7rem] md:text-[1rem] rounded-xl md:rounded-2xl text-white hover:bg-[#91572d]">Order Now</button></Link>
+              <Link href={"/flavors"}><button className="p-[0.5rem] md:p-[0.7rem] bg-[#8B4513] text-[0.7rem] md:text-[1rem] rounded-xl md:rounded-2xl text-white hover:bg-[#91572d]">More Flavor</button></Link>
+            </div>
+          </div>
+
+          <div className="relative z-10 w-full max-w-[500px] h-[250px] mx-auto mt-[5.5rem] overflow-visible hidden md:block">
+            <div className="absolute inset-0 border-t-2 border-[#8B4513] rounded-t-full w-full h-full"></div>
+            <div className="absolute inset-0 rounded-t-full">
+              <div className="absolute left-0 top-[100px] flex flex-col items-center">
+                <Image src={strawberry} alt="Strawberry" className="w-[4rem] h-[4rem] shake-on-hover rounded-full transition-transform duration-300 hover:scale-105 cursor-pointer custom-shadow-hover" />
+                <span className="text-xs text-gray-600 rotate-[-75deg] mt-[2rem]"></span>
+              </div>
+              <div className="absolute left-[120px] top-[-10px] flex flex-col items-center">
+                <Image src={chocolate} alt="Chocolate" className="w-[3.3rem] h-[3.5rem] shake-on-hover rounded-full transition-transform duration-300 hover:scale-105 cursor-pointer custom-shadow-hover" />
+                <span className="text-xs text-gray-600"></span>
+              </div>
+              <div className="absolute right-[120px] top-[-30px] flex flex-col items-center">
+                <Image src={blue_berry} alt="Blueberry" className="w-[4.5rem] h-[4.5rem] shake-on-hover rounded-full transition-transform duration-300 hover:scale-105 cursor-pointer custom-shadow-hover" />
+                <span className="text-xs text-gray-600"></span>
+              </div>
+              <div className="absolute right-[-10px] top-[70px] flex flex-col items-center">
+                <Image src={vanilla} alt="Vanilla" className="w-[5rem] h-[5rem] shake-on-hover rounded-full transition-transform duration-300 hover:scale-105 cursor-pointer custom-shadow-hover" />
+                <span  className="text-xs text-gray-600 mb-2"></span>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+
+        <div className="w-full md:w-1/2 flex items-center justify-center p-4">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src={ice_cream}
+            alt="Ice Cream"
+            className="w-[60%] md:w-full max-w-[370px] object-contain"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+      </section>
     </div>
   );
 }
